df3d46a4afad6e0386d3821d20a3d6520db8b9ed
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-06-30 18:41:57 -0400

    Added context to AccountService

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,6 +1,13 @@
-﻿namespace BankRestApi.Services;
+﻿using BankRestApi.Models;
+
+namespace BankRestApi.Services;
 
 public class AccountService
 {
-    
+    private readonly AccountContext _context;
+
+    public AccountService(AccountContext context)
+    {
+        _context = context;
+    }
 }
