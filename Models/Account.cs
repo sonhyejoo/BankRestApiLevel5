5f68b411f5a0001860bc6f95340dbaac1ed1dd44
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-06-30 16:51:46 -0400

    Added Account model

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace BankRestApi.Models;
+
+public class Account(Guid id, string name, decimal balance)
+{
+    public int InternalId { get; set; }
+    public Guid Id { get; } = id;
+    public string Name { get; set; } = name;
+    public decimal Balance { get; set; } = balance;
+}
