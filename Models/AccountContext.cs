fc81863c24ad945a073fac2307616594042bf345
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-06-30 16:59:02 -0400

    Added AccountContext model

---
+++ Models/AccountContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankRestApi.Models;
+
+public class AccountContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; } = null!;
+
+    public AccountContext(DbContextOptions<AccountContext> options)
+        : base(options)
+    {
+    }
+}
