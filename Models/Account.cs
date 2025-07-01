0b272a252b27cf7a5ba25025d1a701bf31b1c54f
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-01 12:54:57 -0400

    Removed InternalId from Models/Account

--- Models/Account.cs
+++ Models/Account.cs
@@ -4,8 +4,6 @@ namespace BankRestApi.Models;
 
 public class Account
 {
-    [Key]
-    public int InternalId { get; set; }
     public Guid Id { get; set; }
     public string Name { get; set; } 
     public decimal Balance { get; set; }
