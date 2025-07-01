2f5cdd26d4aed1f2b51ff4e392266e0172d051f3
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-01 10:18:24 -0400

    Refactored DTOs/Account from record to class

--- Models/DTOs/Account.cs
+++ Models/DTOs/Account.cs
@@ -1,7 +1,8 @@
 ﻿namespace BankRestApi.Models.DTOs;
 
-public record Account(
-    Guid Id,
-    string Name,
-    decimal Balance
-    );
+public class Account
+{
+    public Guid Id { get; set; } = Guid.Empty;
+    public string Name { get; set; } = "";
+    public decimal Balance { get; set; } = 0;
+}
