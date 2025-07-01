ae7796bc1a7c588e837854575d5bcc461a7d1165
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-01 10:36:09 -0400

    Added Create() to AccountService

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,4 +1,6 @@
 ﻿using BankRestApi.Models;
+using BankRestApi.Models.DTOs;
+using Account = BankRestApi.Models.DTOs.Account;
 
 namespace BankRestApi.Services;
 
@@ -9,5 +11,42 @@ public class AccountService
     public AccountService(AccountContext context)
     {
         _context = context;
+    }
+
+    async Task<AccountResult<Account>> Create(CreateAccountRequest request)
+    {
+        var name = request.Name;
+        if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+        {
+            return new AccountResult<Account>
+            (
+                result: new Account{
+                    Id = Guid.Empty,
+                    Name = "",
+                    Balance = 0
+                },
+                errorMessage: "Name cannot be empty or whitespace."
+            );
+        }
+
+        var accountToAdd = new Models.Account
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Balance = 0
+        };
+        
+        _context.Accounts.Add(accountToAdd);
+        await _context.SaveChangesAsync();
+
+        return new AccountResult<Account>
+        (
+            result: new Account
+            {
+                Id = accountToAdd.Id,
+                Name = accountToAdd.Name,
+                Balance = accountToAdd.Balance
+            }
+        );
     }
 }
