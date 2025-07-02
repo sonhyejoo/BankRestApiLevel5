2b2c763200d48a43ae97665888575eb663d85e3d
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-02 14:37:34 -0400

    Removed result as parameter for errors

--- Models/DTOs/AccountResult.cs
+++ Models/DTOs/AccountResult.cs
@@ -16,14 +16,14 @@ public class AccountResult<T>
         ErrorMessage = errorMessage;
     }
 
-    public static AccountResult<T> GreaterThanZeroError(T result) =>
-        new(result, "Please enter valid decimal amount greater than zero.");
-    public static AccountResult<T> NotFoundError(T result) =>
-        new(result, "No account found with that ID.");
-    public static AccountResult<T> InsufficientFundsError(T result) =>
-        new(result, "Insufficient funds.");
-    public static AccountResult<T> EmptyNameError(T result) =>
-        new(result, "Name cannot be empty or whitespace.");
-    public static AccountResult<T> DuplicateIdError(T result) =>
-        new(result, "Duplicate ids given for sender and recipient.");
+    public static AccountResult<T> GreaterThanZeroError() =>
+        new("Please enter valid decimal amount greater than zero.");
+    public static AccountResult<T> NotFoundError() =>
+        new("No account found with that ID.");
+    public static AccountResult<T> InsufficientFundsError() =>
+        new("Insufficient funds.");
+    public static AccountResult<T> EmptyNameError() =>
+        new("Name cannot be empty or whitespace.");
+    public static AccountResult<T> DuplicateIdError() =>
+        new("Duplicate ids given for sender and recipient.");
 }
