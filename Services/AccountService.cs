0e09e68be192058d5dae5f896ab087b72fe07ba5
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-10 10:20:00 -0400

    Removed context from AccountService to prep for AccountRepository injection

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -9,13 +9,6 @@ namespace BankRestApi.Services;
 
 public class AccountService : IAccountService
 {
-    private readonly AccountContext _context;
-
-    public AccountService(AccountContext context)
-    {
-        _context = context;
-    }
-
     public async Task<AccountResult<Account>> Create(CreateAccountRequest request)
     {
         var name = request.Name;
