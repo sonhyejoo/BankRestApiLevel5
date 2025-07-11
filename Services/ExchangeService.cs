c99afcdad2f8a9a8a7f31c533b7f099ffbae24c9
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-11 16:01:43 -0400

    Added logger to ExchangeService

--- Services/ExchangeService.cs
+++ Services/ExchangeService.cs
@@ -6,16 +6,19 @@ public class ExchangeService : IExchangeService
 {
     private readonly HttpClient _httpClient;
     private readonly IConfiguration _config;
-    
-    public ExchangeService(HttpClient httpClient, IConfiguration config)
+    private readonly ILogger<IExchangeService> _logger;
+
+    public ExchangeService(HttpClient httpClient, IConfiguration config, ILogger<IExchangeService> logger)
     {
         _httpClient = httpClient;
         _config = config;
+        _logger = logger;
     }
     public async Task<Dictionary<string, decimal>> GetExchangeRatesAsync(string currencies)
     {
         _httpClient.DefaultRequestHeaders.Add("apikey", _config["apikey"]);
         var response = await _httpClient.GetFromJsonAsync<FreeCurrencyApiResponse>("?currencies=" + currencies);
+        _logger.LogInformation("Response: " + response);
         return response.data;
     }
 }
