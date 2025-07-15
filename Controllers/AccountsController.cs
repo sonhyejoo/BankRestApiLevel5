9e3120f0d766bf52aa669284169a55eafdb5dd5e
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-15 13:31:52 -0400

    Refactored Controllers namespace to remove indent from AccountsController

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -6,137 +6,136 @@ using BankRestApi.Models.DTOs.Requests;
 using BankRestApi.Services;
 using Account = BankRestApi.Models.DTOs.Account;
 
-namespace BankRestApi.Controllers
+namespace BankRestApi.Controllers;
+
+/// <summary>
+/// Controller for managing bank interactions.
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController : ControllerBase
 {
-    /// <summary>
-    /// Controller for managing bank interactions.
-    /// </summary>
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountsController : ControllerBase
+    private readonly IAccountService _service;
+
+    public AccountsController(IAccountService accountService)
     {
-        private readonly IAccountService _service;
+        _service = accountService;
+    }
+    
+    /// <summary>
+    /// Get specific account by unique id
+    /// </summary>
+    /// <param name="id">Account ID.</param>
+    /// <returns>Account details if successful, otherwise returns  NotFound.</returns>
+    // GET: api/Accounts/0b4b7e2b-ffd1-4acf-81b3-e51d48155217
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
+    public async Task<ActionResult<Account>> GetAccount(Guid id)
+    {
+        var request = new GetAccount(id);
+        var result = await _service.Get(request);
 
-        public AccountsController(IAccountService accountService)
+        if (!result.IsSuccess)
         {
-            _service = accountService;
-        }
-        
-        /// <summary>
-        /// Get specific account by unique id
-        /// </summary>
-        /// <param name="id">Account ID.</param>
-        /// <returns>Account details if successful, otherwise returns  NotFound.</returns>
-        // GET: api/Accounts/0b4b7e2b-ffd1-4acf-81b3-e51d48155217
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
-        public async Task<ActionResult<Account>> GetAccount(Guid id)
-        {
-            var request = new GetAccount(id);
-            var result = await _service.Get(request);
-
-            if (!result.IsSuccess)
-            {
-                return NotFound(result.ErrorMessage);
-            }
-
-            return Ok(result.Result);
+            return NotFound(result.ErrorMessage);
         }
 
-        /// <summary>
-        /// Create a new account.
-        /// </summary>
-        /// <param name="request">Name of account holder.</param>
-        /// <returns>Created account details, otherwise BadRequest if name is empty.</returns>
-        // POST: api/Accounts
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created, Type=typeof(Account))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
-        public async Task<ActionResult<Account>> CreateAccount(CreateAccount request)
-        {
-            var result = await _service.Create(request);
-            if (!result.IsSuccess)
-            {
-                return BadRequest(result.ErrorMessage);
-            }
-            var createdAccount = result.Result;
+        return Ok(result.Result);
+    }
 
-            return CreatedAtAction(nameof(GetAccount), new { id = createdAccount.Id }, createdAccount);
+    /// <summary>
+    /// Create a new account.
+    /// </summary>
+    /// <param name="request">Name of account holder.</param>
+    /// <returns>Created account details, otherwise BadRequest if name is empty.</returns>
+    // POST: api/Accounts
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created, Type=typeof(Account))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
+    public async Task<ActionResult<Account>> CreateAccount(CreateAccount request)
+    {
+        var result = await _service.Create(request);
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.ErrorMessage);
         }
+        var createdAccount = result.Result;
 
-        /// <summary>
-        /// Deposit funds into account
-        /// </summary>
-        /// <param name="id">Account ID.</param>
-        /// <param name="amount">Deposit amount.</param>
-        /// <returns>Updated account details if successful, otherwise BadRequest or NotFound.</returns>
-        // POST: api/Accounts/0b4b7e2b-ffd1-4acf-81b3-e51d48155217/deposits
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost("{id}/deposits")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest,  Type=typeof(string))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
-        public async Task<ActionResult<AccountResult<Account>>> DepositAccount(Guid id, [FromBody] decimal amount)
-        {
-            var request = new Transaction(amount, id);
-            var result = await _service.Deposit(request);
-            return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
-        }
-        
-        /// <summary>
-        /// Withdraws funds from an account.
-        /// </summary>
-        /// <param name="id">Account ID.</param>
-        /// <param name="amount">Withdrawal amount.</param>
-        /// <returns>Updated account details if successful, otherwise BadRequest or NotFound.</returns>
-        // POST: api/Accounts/0b4b7e2b-ffd1-4acf-81b3-e51d48155217/withdrawals
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost("{id}/withdrawals")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
-        public async Task<ActionResult<AccountResult<Account>>> WithdrawAccount(Guid id, [FromBody] decimal amount)
-        {
-            var request = new Transaction(amount, id);
-            var result = await _service.Withdraw(request);
-            return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
-        }
-        
-        /// <summary>
-        /// Transfers funds between two accounts.
-        /// </summary>
-        /// <param name="request">Amount to transfer, sender's ID, recipient's ID.</param>
-        /// <returns>Updated accounts' balances if successful, otherwise BadRequest or NotFound.</returns>
-        // POST: api/Accounts/transfers
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost("transfers")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
-        public async Task<ActionResult<TransferDetails>> Transfer(Transaction request)
-        {
-            var result = await _service.Transfer(request);
-            return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
-        }
-        
-        /// <summary>
-        /// Converts account balance value to other currencies
-        /// </summary>
-        /// <param name="id">Account ID to convert balance of</param>
-        /// <param name="request">List of comma separated currency codes</param>
-        /// <returns>Account information and balance in specified currencies (all if not specified)</returns>
-        [HttpPost("{id}/conversion")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(ConvertedBalances))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
-        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity, Type=typeof(string))]
-        public async Task<ActionResult<ConvertedBalances>> GetConvertedBalances(Guid id, ConvertRequest request)
-        {
-            var convertCommand = new ConvertCommand(id, request.Currencies);
-            var result = await _service.ConvertBalances(convertCommand);
-            return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
-        }
+        return CreatedAtAction(nameof(GetAccount), new { id = createdAccount.Id }, createdAccount);
+    }
+
+    /// <summary>
+    /// Deposit funds into account
+    /// </summary>
+    /// <param name="id">Account ID.</param>
+    /// <param name="amount">Deposit amount.</param>
+    /// <returns>Updated account details if successful, otherwise BadRequest or NotFound.</returns>
+    // POST: api/Accounts/0b4b7e2b-ffd1-4acf-81b3-e51d48155217/deposits
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost("{id}/deposits")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest,  Type=typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
+    public async Task<ActionResult<AccountResult<Account>>> DepositAccount(Guid id, [FromBody] decimal amount)
+    {
+        var request = new Transaction(amount, id);
+        var result = await _service.Deposit(request);
+        return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
+    }
+    
+    /// <summary>
+    /// Withdraws funds from an account.
+    /// </summary>
+    /// <param name="id">Account ID.</param>
+    /// <param name="amount">Withdrawal amount.</param>
+    /// <returns>Updated account details if successful, otherwise BadRequest or NotFound.</returns>
+    // POST: api/Accounts/0b4b7e2b-ffd1-4acf-81b3-e51d48155217/withdrawals
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost("{id}/withdrawals")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
+    public async Task<ActionResult<AccountResult<Account>>> WithdrawAccount(Guid id, [FromBody] decimal amount)
+    {
+        var request = new Transaction(amount, id);
+        var result = await _service.Withdraw(request);
+        return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
+    }
+    
+    /// <summary>
+    /// Transfers funds between two accounts.
+    /// </summary>
+    /// <param name="request">Amount to transfer, sender's ID, recipient's ID.</param>
+    /// <returns>Updated accounts' balances if successful, otherwise BadRequest or NotFound.</returns>
+    // POST: api/Accounts/transfers
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost("transfers")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(Account))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
+    public async Task<ActionResult<TransferDetails>> Transfer(Transaction request)
+    {
+        var result = await _service.Transfer(request);
+        return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
+    }
+    
+    /// <summary>
+    /// Converts account balance value to other currencies
+    /// </summary>
+    /// <param name="id">Account ID to convert balance of</param>
+    /// <param name="request">List of comma separated currency codes</param>
+    /// <returns>Account information and balance in specified currencies (all if not specified)</returns>
+    [HttpPost("{id}/conversion")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(ConvertedBalances))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type=typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type=typeof(string))]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity, Type=typeof(string))]
+    public async Task<ActionResult<ConvertedBalances>> GetConvertedBalances(Guid id, ConvertRequest request)
+    {
+        var convertCommand = new ConvertCommand(id, request.Currencies);
+        var result = await _service.ConvertBalances(convertCommand);
+        return result.IsSuccess ? Ok(result.Result) : StatusCode((int)result.StatusCode!, result.ErrorMessage);
     }
 }
