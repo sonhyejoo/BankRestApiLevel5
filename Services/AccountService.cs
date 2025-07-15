e8366e9d190d7dcb621af88ab618517cfeb65e21
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-15 13:46:33 -0400

    Removed redundant string.IsNullOrEmpty call

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -24,7 +24,7 @@ public class AccountService : IAccountService
     {
         var name = request.Name;
         
-        if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+        if (string.IsNullOrWhiteSpace(name))
         {
             return AccountResult<Account>.EmptyNameError();
         }
