25efb98f52cbfb665afbe5d18def0e8704d059c2
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-17 09:06:56 -0400

    Added ExchangeRateResult to use as return type of GetExchangeRatesAsync httpClient

---
+++ Models/DTOs/ExchangeRateResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace BankRestApi.Models.DTOs;
+
+public record ExchangeRateResult(
+    HttpStatusCode StatusCode,
+    Dictionary<string, decimal>? ExchangeRates,
+    string ErrorMessage = "")
+{
+    public ExchangeRateResult(
+        HttpStatusCode StatusCode,
+        string ErrorMessage)
+        : this(StatusCode, null, ErrorMessage) { }
+}
