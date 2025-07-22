b1b7399bc06b28af5168a530f6cda14c0a332b49
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-22 17:17:16 -0400

    Added PaginationMetadata class, WIP: creating AccountResult constructor for it

--- Repositories/AccountRepository.cs
+++ Repositories/AccountRepository.cs
@@ -1,6 +1,8 @@
 ﻿using BankRestApi.Interfaces;
 using BankRestApi.Models;
+using BankRestApi.Models.DTOs;
 using Microsoft.EntityFrameworkCore;
+using Account = BankRestApi.Models.Account;
 
 namespace BankRestApi.Repositories;
 
@@ -13,36 +15,43 @@ public class AccountRepository : IAccountRepository
         _context = context;
     }
 
-    public async Task<IEnumerable<Account>> GetAccounts(
+    public async Task<(IEnumerable<Account>, PaginationMetadata)> GetAccounts(
         string? name,
         string sort,
         bool desc,
         int pageNumber,
         int pageSize)
     {
-        var result = _context.Accounts as IQueryable<Account>;
+        var queryBuilder = _context.Accounts as IQueryable<Account>;
         
         if (!string.IsNullOrWhiteSpace(name))
         {
             name = name.Trim();
-            result = result.Where(a => a.Name == name);
+            queryBuilder = queryBuilder.Where(a => a.Name == name);
         }
 
         sort = sort.Trim().ToLower();
         switch (sort)
         {
             case "name":
-                result = result.OrderBy(a => a.Name);
+                queryBuilder = queryBuilder.OrderBy(a => a.Name);
                 break;
             case "balance":
-                result = result.OrderBy(a => a.Balance);
+                queryBuilder = queryBuilder.OrderBy(a => a.Balance);
                 break;
         }
 
-        result = result
+        var totalItemCount = await queryBuilder.CountAsync();
+        var paginationMetaData = new PaginationMetadata(totalItemCount, pageSize, pageNumber);
+
+        queryBuilder = queryBuilder
             .Skip(pageSize * (pageNumber - 1))
             .Take(pageSize);
-        return desc ? await result.Reverse().ToListAsync() : await result.ToListAsync();
+        var result = desc 
+            ? await queryBuilder.Reverse().ToListAsync() 
+            : await queryBuilder.ToListAsync();
+
+        return (result, paginationMetaData);
     }
 
     public Task<Account?> GetById(Guid? id)
