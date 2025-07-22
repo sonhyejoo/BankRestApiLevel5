d6797854b2be786816e7131673f0ee517d199c38
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-22 16:49:05 -0400

    Added pagination, sorting, filtering on AccountsController.GetAccounts endpoint

--- Interfaces/IAccountRepository.cs
+++ Interfaces/IAccountRepository.cs
@@ -4,7 +4,12 @@ namespace BankRestApi.Interfaces;
 
 public interface IAccountRepository
 {
-    Task<IEnumerable<Account>> GetAccounts();
+    Task<IEnumerable<Account>> GetAccounts(
+        string? name,
+        string sort,
+        bool desc,
+        int pageNumber,
+        int pageSize);
     
     Task<Account?> GetById(Guid? id);
     
