84f3436a834ae5e741c31628ff11b3307e951443
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-07-25 17:01:41 -0400

    Added jwt return to authentication endpoint

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,6 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using BankRestApi.Models;
 using BankRestApi.Models.DTOs.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BankRestApi.Controllers
 {
@@ -8,15 +13,52 @@ namespace BankRestApi.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        // [HttpPost("authenticate")]
-        // public ActionResult<string> Authenticate(AuthenticationRequest request)
-        // {
-        //     var user = ValidateUserCredentials(request.AccountName, request.Password);
-        // }
-        //
-        // private object ValidateUserCredentials(string requestAccountName, string requestPassword)
-        // {
-        //     throw new NotImplementedException();
-        // } 
+        private readonly AccountContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthenticationController(AccountContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        
+        [HttpPost("authenticate")]
+        public async Task<ActionResult<string>> Authenticate(AuthenticationRequest request)
+        {
+            var user = await ValidateUserCredentials(request.AccountName, request.Password);
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            var securityKey = new SymmetricSecurityKey(
+                Convert.FromBase64String(_config["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>()
+            {
+                new Claim("sub", user.Id.ToString()),
+                new Claim("name", user.AccountName)
+            };
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _config["Authentication:Issuer"],
+                _config["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+        
+        private async Task<User?> ValidateUserCredentials(string accountName, string password)
+        {
+            var currentUser = await _context.Users.FirstOrDefaultAsync(u => u.AccountName == accountName);
+            return currentUser is null || currentUser.HashedPassword != password ? null : currentUser;
+        } 
     }
 }
