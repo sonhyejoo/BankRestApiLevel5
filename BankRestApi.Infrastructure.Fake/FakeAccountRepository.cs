acee6e362348039a81e235a623f51b922048abae
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-08-14 11:16:16 -0400

    Added fake accountRepository and exchangeService for testing

---
+++ BankRestApi.Infrastructure.Fake/FakeAccountRepository.cs
@@ -0,0 +1,82 @@
+﻿using BankRestApi.Application.DTOs.Requests;
+using BankRestApi.Application.DTOs.Results;
+using BankRestApi.Application.Interfaces;
+using BankRestApi.Domain.Entities;
+
+namespace BankRestApi.Infrastructure.Fake;
+
+public class FakeAccountRepository : IAccountRepository
+{
+    private static readonly Account? NullAccount = null;
+
+    private readonly Dictionary<Guid, Account> _accounts;
+
+    public FakeAccountRepository()
+    {
+        _accounts = new Dictionary<Guid, Account>();
+    }
+    
+    public Task<(IEnumerable<Account>, PaginationMetadata)> GetAccounts(GetAccountsQueryParameters queryParameters)
+    {
+        var (name, sortBy, desc, pageNumber, pageSize) = queryParameters;
+        var queryBuilder = _accounts.Values.AsQueryable();
+        
+        if (!string.IsNullOrWhiteSpace(queryParameters.Name))
+        {
+            name = name.Trim();
+            queryBuilder = queryBuilder.Where(a => a.Name == name);
+        }
+
+        sortBy = sortBy.Trim().ToLower();
+        queryBuilder = sortBy switch
+        {
+            "name" => queryBuilder.OrderBy(a => a.Name),
+            "balance" => queryBuilder.OrderBy(a => a.Balance),
+            _ => queryBuilder
+        };
+
+        var totalItemCount = queryBuilder.Count();
+        var pageData = new PaginationMetadata(totalItemCount, pageSize, pageNumber);
+
+        queryBuilder = queryBuilder
+            .Skip(pageSize * (pageNumber - 1))
+            .Take(pageSize);
+        var result = desc 
+            ? queryBuilder.Reverse().ToList() 
+            : queryBuilder.ToList();
+
+        return Task.FromResult<(IEnumerable<Account>, PaginationMetadata)>((result, pageData));
+
+    }
+
+    public Task<Account?> GetById(Guid? id)
+    {
+        if (!_accounts.ContainsKey((Guid)id))
+        {
+            return Task.FromResult(NullAccount);
+        }
+        
+        return Task.FromResult(_accounts.FirstOrDefault(kvp => kvp.Key == (Guid)id).Value);
+    }
+
+    public Task<Account?> Add(string name)
+    {
+        var accountToAdd = new Account
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Balance = 0
+        };
+        _accounts.Add(accountToAdd.Id, accountToAdd);
+
+        return Task.FromResult(accountToAdd);
+    }
+
+    public Task<Account?> Update(Account account, decimal amount)
+    {
+        account.Balance += amount;
+        _accounts[account.Id] = account;
+
+        return Task.FromResult(account);
+    }
+}
---
+++ BankRestApi.Infrastructure.Fake/FakeExchangeService.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using BankRestApi.Application.DTOs.Results;
+using BankRestApi.Application.Interfaces;
+
+namespace BankRestApi.Infrastructure.Fake;
+
+public class FakeExchangeService : IExchangeService
+{
+    private List<string> _validCurrencies =
+    [
+        "EUR",
+        "USD",
+        "JPY",
+        "BGN",
+        "CZK",
+        "DKK",
+        "GBP",
+        "HUF",
+        "PLN",
+        "RON",
+        "SEK",
+        "CHF",
+        "ISK",
+        "NOK",
+        "HRK",
+        "RUB",
+        "TRY",
+        "AUD",
+        "BRL",
+        "CAD",
+        "CNY",
+        "HKD",
+        "IDR",
+        "ILS",
+        "INR",
+        "KRW",
+        "MXN",
+        "MYR",
+        "NZD",
+        "PHP",
+        "SGD",
+        "THB",
+        "ZAR"
+    ];
+
+    public Task<ExchangeRateResult> GetExchangeRates(string currencies)
+    {
+        var splitCurrencies = currencies.Split(",");
+        if (splitCurrencies.Any(currency => _validCurrencies.Contains(currency)))
+        {
+            return Task.FromResult(new ExchangeRateResult(HttpStatusCode.UnprocessableEntity, "Invalid currencies inputted."));
+        }
+
+        var result = splitCurrencies.ToDictionary(
+            currency => currency,
+            _ => (decimal) 0);
+
+        return Task.FromResult(new ExchangeRateResult(HttpStatusCode.OK, currencies));
+    }
+}
